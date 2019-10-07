5312bcaf2ad2b733d519b6c20ed5d98aac4a6cf2
Rafael Barash <rafabarash@gmail.com>
2019-10-07 11:44:21 -0500

    changed router to mux because there is much more documentation with using mux online

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	handler := http.NewServeMux()              // router to expose api
+	// handler := http.NewServeMux()              // router to expose api
+	handler := mux.NewRouter()
 	handler.HandleFunc("/api/hello", SayHello) // hello world endpoint
 	fmt.Println("Server starting on localhost:8080...")
 	http.ListenAndServe(":8080", handler) // listen to requests on localhost:8080
