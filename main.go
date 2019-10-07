27701ca06c94e24c468518cbabc4690ef9493033
Rafael Barash <rafabarash@gmail.com>
2019-10-07 10:59:05 -0500

    basic hello world rest api

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handler := http.NewServeMux()              // router to expose api
+	handler.HandleFunc("/api/hello", SayHello) // hello world endpoint
+	fmt.Println("Server starting on localhost:8080...")
+	http.ListenAndServe(":8080", handler) // listen to requests on localhost:8080
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!\n")
+}
