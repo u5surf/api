8afb14a752b2e75b756f2758f44657a636f655a9
Rafael Barash <rafabarash@gmail.com>
2019-10-09 11:07:12 -0500

    added dynamic GET enpoint for getting honeyclient at /api/honeyclient/{id} (#2). Refactored POST endpoint for creating honeyclient from /api/create-client to /api/honeyclient/create (#3)

--- main.go
+++ main.go
@@ -5,32 +5,40 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type ScreenShot struct {
-	Width    int64  `json:"width"`
-	Height   int64  `json:"height"`
+	Width    int    `json:"width"`
+	Height   int    `json:"height"`
 	Filename string `json:"filename"`
 }
 
 type Ticket struct {
-	ID         int64      `json:"id"`
+	ID         int        `json:"id"`
 	Name       string     `json:"name"`
 	URL        string     `json:"url"`
 	Screenshot ScreenShot `json:"screenshot"`
 }
 
 func main() {
-	handler := mux.NewRouter()                             // create router for handling api endpoints
-	handler.HandleFunc("/api/create-client", CreateClient) // POST endpoint for creating honeyclient
+	handler := mux.NewRouter()                                       // create router for handling api endpoints
+	handler.HandleFunc("/api/honeyclient/create", CreateHoneyClient) // POST endpoint for creating honeyclient
+	handler.HandleFunc("/api/honeyclient/{id}", GetHoneyClientById)  // GET endpoint for getting honeyclient by id
 	fmt.Println("Server starting on localhost:8080...")
 	http.ListenAndServe(":8080", handler) // listen to requests on localhost:8080
 }
 
-func CreateClient(respond http.ResponseWriter, request *http.Request) {
+func CreateHoneyClient(w http.ResponseWriter, r *http.Request) {
 	var ticket Ticket
-	json.NewDecoder(request.Body).Decode(&ticket)
-	ticket.ID, ticket.Name = 1, "Dummy ticket"
-	fmt.Fprintf(respond, "Ticket '%s' (%d) will investigate url '%s' with screenshot dimensions %d x %d", ticket.Name, ticket.ID, ticket.URL, ticket.Screenshot.Width, ticket.Screenshot.Height)
+	json.NewDecoder(r.Body).Decode(&ticket)    // decodes json from request into Ticket struct
+	ticket.ID, ticket.Name = 1, "Dummy ticket" // ID and Name are not part of the request
+	fmt.Fprintf(w, "Ticket '%s' (%d) will investigate url '%s' with screenshot dimensions %d x %d", ticket.Name, ticket.ID, ticket.URL, ticket.Screenshot.Width, ticket.Screenshot.Height)
+}
 
+func GetHoneyClientById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)               // get dynamic variables from mux handler
+	id, _ := strconv.Atoi(vars["id"]) // get integer "ID" from vars
+	ticket := Ticket{ID: id, Name: "Hardcoded ticket", URL: "https://example.com"}
+	fmt.Fprintf(w, "Fetched ticket '%s' from ID '%d' with URL '%s'", ticket.Name, ticket.ID, ticket.URL)
 }
