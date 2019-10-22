58f7e518207322456ab9f06cd5e3fb444181c597
William Teder <wteder@hydreon.com>
2019-10-22 11:34:43 -0500

    feat(postgres): initial gorm integration

--- models/screenshot.go
+++ models/screenshot.go
@@ -1,6 +1,9 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type ScreenShot struct {
+	gorm.Model
 	Width    int    `json:"width"`
 	Height   int    `json:"height"`
 	Filename string `json:"filename"`
