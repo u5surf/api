e1df76126ba0fac73fbe71e4b8ab7200ddc7eb25
William Teder <wteder@hydreon.com>
2019-10-22 11:34:43 -0500

    feat(postgres): initial gorm integration

--- models/ticket.go
+++ models/ticket.go
@@ -1,8 +1,11 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type Ticket struct {
-	ID         int        `json:"id"`
-	Name       string     `json:"name"`
-	URL        string     `json:"url"`
+	gorm.Model
+	ID         int        `json:"id",gorm:"AUTO_INCREMENT;PRIMARY_KEY"`
+	Name       string     `json:"name",gorm:"size:255"`
+	URL        string     `json:"url"gorm:"size:4096"`
 	Screenshot ScreenShot `json:"screenshot"`
 }
