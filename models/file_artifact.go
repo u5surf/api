ad83e251b98112a1d297c1d3f91991079ffecb4a
William Teder <wteder@hydreon.com>
2019-10-23 18:29:11 -0500

    fix(postgres): primary key definition

--- models/file_artifact.go
+++ models/file_artifact.go
@@ -1,7 +1,7 @@
 package models
 
 type FileArtifact struct {
-	TicketId uint   `json:"ticketID" gorm:"primary_key" sql:"not null"`
-	Filename string `json:"filename" gorm:"primary_key,size:255" sql:"not null"`
+	TicketId uint   `json:"ticketID" gorm:"primary_key"`
+	Filename string `json:"filename" gorm:"primary_key"`
 	Data     []byte
 }
