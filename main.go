6c0ead556a8a90fe3d4b9e1294c8f351b5aa5239
Rafi Barash <rafabarash@gmail.com>
2019-10-27 20:02:11 -0500

    removed redundent db message

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 	handler.HandleFunc("/api/honeyclient/create", routes.CreateHoneyClient) // POST endpoint for creating honeyclient
 	handler.HandleFunc("/api/honeyclient/{id}", routes.GetHoneyClientById)  // GET endpoint for getting honeyclient by id
 
-	fmt.Println("Connecting to the database...")
 	models.InitDB("host=127.0.0.1 port=5432 user=gorm dbname=gorm password=gorm sslmode=disable")
 
 	fmt.Println("Server starting...")
