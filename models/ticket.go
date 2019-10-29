0c68dce8433863026ee2651694a12cfe96ff8746
Rafael Barash <rafabarash@gmail.com>
2019-10-29 16:21:48 -0500

    added screenshot field back to Ticket struct for now... this is not working as intended so will have to fix later (#34)

--- models/ticket.go
+++ models/ticket.go
@@ -6,9 +6,10 @@ import (
 
 type Ticket struct {
 	gorm.Model
-	Name      string `json:"name",gorm:"size:255"`
-	URL       string `json:"url",gorm:"size:4096"`
-	Processed bool   `json:"processed"`
+	Name       string       `json:"name",gorm:"size:255"`
+	URL        string       `json:"url",gorm:"size:4096"`
+	Processed  bool         `json:"processed"`
+	ScreenShot []ScreenShot `json:"screenshots"`
 }
 
 func CreateTicket(ticket *Ticket) error {
@@ -17,7 +18,8 @@ func CreateTicket(ticket *Ticket) error {
 
 func GetTicketById(ID uint) (*Ticket, error) {
 	var ticket Ticket
-	err := db.First(&ticket, ID).Error
+	// Preload line fetches the screenshot table and joins automatically:
+	err := db.Preload("ScreenShot").First(&ticket, ID).Error
 
 	return &ticket, err
 }
