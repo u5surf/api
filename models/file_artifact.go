6f22f6060f502e3603d9dfa8eaaedb9de3859fb5
William Teder <wteder@hydreon.com>
2019-11-05 01:26:02 -0600

    feat(routes.artifacts): get artifact endpoint

--- models/file_artifact.go
+++ models/file_artifact.go
@@ -9,7 +9,15 @@ type FileArtifact struct {
 func GetAssociatedArtifacts(TicketID uint) (*[]FileArtifact, error) {
 	var fileArtifacts []FileArtifact
 
-	err := db.Where("ticket_id = ?", TicketID).Find(&fileArtifacts, TicketID).Error
+	err := db.Where("ticket_id = ?", TicketID).Find(&fileArtifacts).Error
 
 	return &fileArtifacts, err
+}
+
+func GetArtifact(TicketID uint, FileName string) (*FileArtifact, error) {
+	var fileArtifact FileArtifact
+
+	err := db.Where("ticket_id = ? AND filename = ?", TicketID, FileName).First(&fileArtifact).Error
+
+	return &fileArtifact, err
 }
